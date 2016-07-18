de8efd58c73eb7ba7349704498bc99cb71feb1b2
Ryan Harmuth <ryanharmuth@gmail.com>
2016-07-18 22:40:01 +0700

    Missing constants files from last commit

---
+++ constants/redisChannels.js
@@ -0,0 +1,8 @@
+var RedisChannels = {
+	Message: 'messageChannel',
+    MessageDeliveredConfirmation: 'messageDeliveredConfirmationChannel',
+    OnlineStatus: 'onlineStatusChannel',
+    TypingStatus: 'typingStatusChannel',
+}
+
+module.exports = RedisChannels
